6459cd8b4c4c09e13ddb6515c96e320d82b691c1
Mickaelle Pazuelo <mickaelle.pazuelo@gmail.com>
2021-06-30 15:07:10 +0200

    iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 // Iteration 1: Names and Input
+var hacker1 = "Micka";
+console.log("The driver's name is " + hacker1);
 
+var hacker2 = "Chenjia";
+console.log("The driver's name is " + hacker2);
 
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
