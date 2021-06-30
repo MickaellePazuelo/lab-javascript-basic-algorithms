fd865f183a504d6962a6813ae21a51492bb76c96
Chenjia Li <cligrenet@gmail.com>
2021-06-30 16:36:36 +0200

    iteration 3.3 done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // Iteration 1: Names and Input
-var hacker1 = 'Micka';
+const hacker1 = 'Micka';
 console.log("The driver's name is " + hacker1);
 
-var hacker2 = 'Chenjia';
+const hacker2 = 'Chenjia';
 console.log("The navigator's name is " + hacker2);
 
 // Iteration 2: Conditionals
@@ -24,13 +24,18 @@ if (hacker1.length > hacker2.length) {
 // Iteration 3: Loops
 // 3.1 C A P I T A L  & SPACE BTW
 var str = `${hacker1}`;
-var res = str.toUpperCase() + " ";
-console.log(res);
-
-console.log (res.split("").join(" "));
+console.log(str.toUpperCase() + ' ');
 
 //3.2 REVERSE LETTER
 var str2 = `${hacker2}`;
-console.log(str2.split("").reverse("").join(""));
+console.log(str2.split('').reverse('').join(''));
 
 //3.3
+if (hacker1.localeCompare(hacker2) === 1) {
+  console.log(`The driver's name goes first.`);
+} else if (hacker1.localeCompare(hacker2) === -1) {
+  console.log(`Yo, the navigator goes first definitely.`);
+} else {
+  console.log(`What?! You both have the same name?
+  `);
+}
